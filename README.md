3b7464161a995e48a4110ca8413d730c6a6c5788
Fabian Cook <faybecook@gmail.com>
2018-06-11 21:10:52 +1200

    Get state inc working

---
+++ README.md
@@ -0,0 +1,64 @@
+# Contribution Graph Bot
+
+A "bot" that pushes to a github account each day to create a graphic using the contribution graph. 
+
+The intention of the "bot" is to learn how to use both AWS lambda and the GitHub API. Please do not set this running on 
+your profile just to get a fully green contribution graph, that would be dishonest. 
+
+## How to run
+
+Fork this project to your bots account (explained below).
+
+You will need `node`, the `aws cli`, the `serverless cli` and `yarn`. 
+
+You will also need a personal token for a github account, make a specific account for this so you don't change your main 
+contribution graph, it needs to have access to "Private Repositories"
+
+Once you have these dependencies invoke these commands in your CLI (Command Line Interface)
+
+```bash
+aws kms create-key
+```
+
+The above will give you a key with the value we want, the key is `Arn`, you will use this in the next steps
+
+```bash
+aws kms encrypt --key-id '<arn from previous step>' --plaintext '<github token>'
+```
+
+The above will give you a key with the value we want, the key is `CiphertextBlob`, you will use this in the deploy step
+
+```bash
+yarn install
+```
+
+Then run:
+
+```bash
+serverless deploy --github-token="kms:<encrypted token>" --github-user=<your new bot user> --github-repo=<this forked repo> --kms-key=<ARN For KMS key> --github-timezone=<Your bot profiles timezone>
+```
+
+Your bot will now run a few times a day to make sure your 
+
+## Art Setup 
+
+Modify `art.js` with these values related to the amount of commits to make in a single
+day for a given timezone
+
+0. ` `
+1. `░`
+2. `▒`
+3. `▓`
+4. `█`
+
+Each line in your art should be 52 characters long.
+
+This is the starting point that I am working with
+
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
+░▓░▓░▓▓▓░▓░░░▓░░░▓▓▓░░░░▓░░░▓░▓▓▓░▓▓▓░▓░░░▓▓░░░░░░░░
+░▓░▓░▓░░░▓░░░▓░░░▓░▓░░░░▓░░░▓░▓░▓░▓░▓░▓░░░▓░▓░░░░░░░
+░▓▓▓░▓▓▓░▓░░░▓░░░▓░▓░░░░▓░▓░▓░▓░▓░▓▓░░▓░░░▓░▓░░░░░░░
+░▓░▓░▓░░░▓░░░▓░░░▓░▓░░░░▓░▓░▓░▓░▓░▓░▓░▓░░░▓░▓░░░░░░░
+░▓░▓░▓▓▓░▓▓▓░▓▓▓░▓▓▓░░░░▓▓▓▓▓░▓▓▓░▓░▓░▓▓▓░▓▓░░░░░░░░
+░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░░
--- run.js
+++ run.js
@@ -2,13 +2,23 @@ import Moment from 'moment-timezone';
 import Art from './art';
 import AWS from 'aws-sdk';
 import Assert from 'assert';
+import fetch from 'node-fetch';
 
 const KMS = new AWS.KMS();
 
+const CURRENT_STATE = Symbol('Current State');
+
 async function getDecryptedValue(value) {
-  if (value.indexOf('arn') !== 0) {
+  if (value.indexOf('kms:') !== 0) {
     return value;
   }
+  const encrypted = value.split(':').slice(1).join(':');
+  const encryptedBuffer = new Buffer(encrypted, 'base64');
+  return new Promise(
+    (resolve, reject) => {
+      KMS.decrypt({ CiphertextBlob: encryptedBuffer }, (error, data) => error ? reject(error) : resolve(data.Plaintext.toString('ascii')))
+    }
+  );
 }
 
 /**
@@ -39,14 +49,107 @@ async function getEnvironmentVariables() {
   );
 }
 
+/**
+ * @param {EnvVariables} variables
+ * @returns {Promise.<ArtStateInfo>|ArtStateInfo}
+ */
+async function getCurrentState(variables) {
+  const response = await fetch(
+    `https://api.github.com/repos/${encodeURIComponent(variables.GITHUB_USER)}/${encodeURIComponent(variables.GITHUB_REPO)}/contents/${encodeURIComponent(variables.GITHUB_STATE_FILE)}`,
+    {
+      method: 'GET',
+      headers: {
+        Authorization: `token ${variables.GITHUB_TOKEN}`,
+        Accept: 'application/json'
+      }
+    }
+  );
+  const info = await response.json();
+  console.log(info, `https://api.github.com/repos/${variables.GITHUB_USER}/${variables.GITHUB_REPO}/contents/${variables.GITHUB_STATE_FILE}`);
+  Assert(typeof info === 'object' && !Array.isArray(info), 'Expected an json object descriptor for the state file');
+  Assert(info.type === 'file', `Expected type of state file to be "file", received "${info.type}"`);
+  Assert(typeof info.sha === 'string' && !!info.sha, 'Expected sha');
+  const data = new Buffer(info.content, info.encoding);
+  const json = data.toString('utf8');
+  const state = JSON.parse(json) || {};
+  // Re-stringify it, so we can ignore any formatting that may be present
+  state[CURRENT_STATE] = JSON.stringify(state);
+  return {
+    state,
+    sha: info.sha
+  };
+}
+
+/**
+ * @param {EnvVariables} variables
+ * @param {ArtState} state
+ * @param {string} sha
+ * @returns {Promise.<void>}
+ */
+async function saveState(variables, state, sha) {
+  // Nothing changed, don't update
+  if (state[CURRENT_STATE] === JSON.stringify(state)) {
+    return null;
+  }
+  const json = JSON.stringify(state, null, '  ');
+  const base64 = new Buffer(json, 'utf8').toString('base64');
+
+  const response = await fetch(
+    `https://api.github.com/repos/${encodeURIComponent(variables.GITHUB_USER)}/${encodeURIComponent(variables.GITHUB_REPO)}/contents/${encodeURIComponent(variables.GITHUB_STATE_FILE)}`,
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: `token ${variables.GITHUB_TOKEN}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: `Update ${variables.GITHUB_STATE_FILE}`,
+        committer: {
+          name: `Contribution Graph Bot B/O ${variables.GITHUB_USER}`,
+          email: `${variables.GITHUB_USER}@users.noreply.github.com`
+        },
+        content: base64,
+        sha
+      })
+    }
+  );
+
+  const info = await response.json();
+
+  console.log(info, `https://api.github.com/repos/${encodeURIComponent(variables.GITHUB_USER)}/${encodeURIComponent(variables.GITHUB_REPO)}/contents/${encodeURIComponent(variables.GITHUB_STATE_FILE)}`,
+    {
+      method: 'PUT',
+      headers: {
+        Authorization: `token ${variables.GITHUB_TOKEN}`,
+        Accept: 'application/json',
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        message: `Update ${variables.GITHUB_STATE_FILE}`,
+        committer: {
+          name: `Contribution Graph Bot B/O ${variables.GITHUB_USER}`,
+          email: `${variables.GITHUB_USER}@users.noreply.github.com`
+        },
+        content: base64,
+        sha
+      })
+    });
+
+  Assert(!!info.content, 'Expected content details to be returned');
+  Assert(!!info.commit, 'Expected commit details to be returned');
+}
+
 export const handler = async (event, context, cb) => {
-  const variables = await getEnvironmentVariables()
+  const variables = await getEnvironmentVariables(),
+    { state, sha } = await getCurrentState(variables);
 
+  await saveState(variables, state, sha);
 
-
-
+  return {
+    message: 'Complete'
+  }
 };
-
 
 /**
  * @typedef {object} EnvVariables
@@ -54,4 +157,15 @@ export const handler = async (event, context, cb) => {
  * @property {string} GITHUB_USER
  * @property {string} GITHUB_REPO
  * @property {string} GITHUB_STATE_FILE
+ */
+
+/**
+ * @typedef {object} ArtStateInfo
+ * @property {ArtState} state
+ * @property {string} sha
+ */
+
+/**
+ * @typedef {object} ArtState
+ *
  */
